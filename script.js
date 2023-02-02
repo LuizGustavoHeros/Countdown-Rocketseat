52841c4e4a3f9aeb614cc7c8579b4ded0b4b6a03
Luiz Gustavo <102548854+LuizGustavoHeros@users.noreply.github.com>
2023-02-02 17:36:31 +0000

    Texto de informação criado

--- script.js
+++ script.js
@@ -3,24 +3,35 @@ const hoursText = document.querySelector('#hours')
 const minutesText = document.querySelector('#minutes')
 const secondsText = document.querySelector('#seconds')
 
-const newYearDate = new Date('1 mar 2023')
+const newYearsDate = new Date('1 mar 2023')
 
-setInterval(() => {
+setInterval(countdown, 1000)
+
+function countdown() {
+  const { days, hours, minutes, seconds } = precessCountdownValues()
+  renderCountdown({ days, hours, minutes, seconds })
+}
+
+function addPadStart(value) {
+  return value < 10 ? `0${value}` : value
+}
+
+function precessCountdownValues() {
   const currentDate = new Date()
-  const totalSeconds = (newYearDate - currentDate) / 1000
+  const totalSeconds = (newYearsDate - currentDate) / 1000
 
   const days = Math.floor((totalSeconds / 3600) / 24)
   const hours = Math.floor((totalSeconds / 3600) % 24)
   const minutes = Math.floor((totalSeconds / 60) % 60)
   const seconds = Math.floor(totalSeconds % 60)
 
+  return { days, hours, minutes, seconds }
+
+}
+
+function renderCountdown({ days, hours, minutes, seconds }) {
   daysText.innerText = addPadStart(days)
   hoursText.innerText = addPadStart(hours)
   minutesText.innerText = addPadStart(minutes)
   secondsText.innerText = addPadStart(seconds)
-
-}, 1000)
-
-function addPadStart(value) {
-  return value < 10 ? `0${value}` : value
-}
+} 
